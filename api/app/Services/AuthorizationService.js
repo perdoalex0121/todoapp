3bc5cb212f504190a219bd91720f7fb9eae84925
alextelenet <alextelenet0121@gmail.com>
2018-05-29 22:45:01 -0400

    Exception, Rules, Delete, missing Update

---
+++ api/app/Services/AuthorizationService.js
@@ -0,0 +1,17 @@
+'use strict'
+const InvalidAccessException = use('App/Exceptions/InvalidAccessException');
+const ResourceNotExistException = use('App/Exceptions/ResourceNotExistException');
+
+class AuthorizationService {
+	verifyPermission(resource, user) {
+		if(!resource) {
+			throw new ResourceNotExistException();
+		}
+
+		if(resource.user_id !== user.id) {
+			throw new InvalidAccessException(); // Invalid Access
+		}
+	}
+}
+
+module.exports = new AuthorizationService
